223b400fff03e0d5ac1c7e07e74f2c64770c33c6
Carlos Galindo <utm22030587@utma.edu.mx>
2025-03-06 15:34:24 -0600

    feat: set up initial express app

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,1 +1,34 @@
-console.log('Hello World');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import fileupload from 'express-fileupload';
+
+const app = express();
+
+dotenv.config();
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.use(cors({
+    origin: (origin, callback) => {
+        callback(null, true);
+    },
+    credentials: true
+}));
+
+app.use(fileupload({
+    createParentPath: true
+}));
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+    res.send({ message: "Hello World" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
