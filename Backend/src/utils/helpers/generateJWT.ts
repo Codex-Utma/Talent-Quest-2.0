fae397391f7d7f03ee2574fa3cd9b80f05ade7cb
Carlos Galindo <utm22030587@utma.edu.mx>
2025-03-06 16:10:21 -0600

    fix: rename environment variable

--- Backend/src/utils/helpers/generateJWT.ts
+++ Backend/src/utils/helpers/generateJWT.ts
@@ -1,10 +1,10 @@
 import jwt from 'jsonwebtoken';
 
 const generateJWT = (uid: number, name: string, lastName: string, userType: string) => {
-    if (!process.env.JWT_SECRET) {
-        throw new Error('JWT_SECRET is not defined');
+    if (!process.env.JWT_SECRET_KEY) {
+        throw new Error('JWT_SECRET_KEY is not defined');
     }
-    return jwt.sign({ uid: String(uid), name, lastName, userType }, process.env.JWT_SECRET, {
+    return jwt.sign({ uid: String(uid), name, lastName, userType }, process.env.JWT_SECRET_KEY, {
         expiresIn: '12h'
     });
 }
