7d27e5603d0b1f31a87773b55715eb5963ca05f9
Carlos Galindo <utm22030587@utma.edu.mx>
2025-03-06 23:53:40 -0600

    fix: require array of courses id´s

--- Backend/src/controllers/admin/project.controller.ts
+++ Backend/src/controllers/admin/project.controller.ts
@@ -8,9 +8,25 @@ const prisma = new PrismaClient();
 const createProject = async (req: Request, res: Response) => {
     try {
         const { name, description } = req.body;
+        const neededCourses: number[] = req.body.neededCourses;
 
-        if (!name || !description) {
+        if (!name || !description || !neededCourses) {
             return returnResponse(res, 400, "Faltan campos por completar");
+        }
+
+        const courses = await prisma.course.findMany({
+            where: {
+                id: {
+                    in: neededCourses
+                }
+            },
+            select: {
+                id: true
+            }
+        });
+
+        if (courses.length !== neededCourses.length) {
+            return returnResponse(res, 404, "Uno o más cursos no existen");
         }
 
         const project = await prisma.project.create({
@@ -18,6 +34,15 @@ const createProject = async (req: Request, res: Response) => {
                 name,
                 description
             }
+        });
+
+        await prisma.coursesNeeded.createMany({
+            data: courses.map(course => {
+                return {
+                    courseId: course.id,
+                    projectId: project.id
+                }
+            })
         });
 
         return returnResponse(res, 201, "Proyecto creado", project);
