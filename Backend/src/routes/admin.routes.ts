66af71418ecbd0b5c6a4c667b49de5b5034e4170
Carlos Galindo <utm22030587@utma.edu.mx>
2025-03-07 00:29:55 -0600

    feat: add endpoint for getting and adding classes

--- Backend/src/routes/admin.routes.ts
+++ Backend/src/routes/admin.routes.ts
@@ -4,6 +4,7 @@ import formRouter from "./admin/forms.routes";
 import projectRouter from "./admin/project.routes";
 import courseRouter from "./admin/course.routes";
 import moduleRouter from "./admin/module.routes";
+import classRouter from "./admin/class.routes";
 
 const router = Router();
 
@@ -11,5 +12,6 @@ router.use("/form", formRouter);
 router.use("/project", projectRouter);
 router.use("/course", courseRouter);
 router.use("/module", moduleRouter);
+router.use("/class", classRouter);
 
 export default router;
