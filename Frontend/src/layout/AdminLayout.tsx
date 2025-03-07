933d57d55e0b37740104dddf71d54736ca23d4d5
Carlos Galindo <utm22030587@utma.edu.mx>
2025-03-07 15:27:57 -0600

    feat: protect routes based on roles

--- Frontend/src/layout/AdminLayout.tsx
+++ Frontend/src/layout/AdminLayout.tsx
@@ -1,6 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+import useAuth from "../hooks/useAuth";
+import { AxiosInstance } from "../config/axios";
 
 export default function AdminLayout() {
+
+    const navigate = useNavigate();
+
+    const { logout, user } = useAuth();
+
+    useEffect(() => {
+        async function validateAuth() {
+            if (!user || user.userType !== 'admin') {
+                logout();
+                navigate('/');
+                await AxiosInstance.post('/user/logout');
+            }
+        }
+
+        validateAuth();
+    }, [user, navigate, logout]);
+
     return (
         <div>
             <p>admin</p>
