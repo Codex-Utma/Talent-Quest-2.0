a173cae87087d1d4ec4c95466d4a592756f89fe4
Carlos Galindo <utm22030587@utma.edu.mx>
2025-03-12 22:13:00 -0600

    refactor: create routes saving params in uri

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,72 +2,67 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
 import AdminLayout from './layout/AdminLayout.tsx'
 import EmployeeLayout from './layout/EmployeeLayout.tsx'
+
 import Login from './Pages/Login.tsx'
-import Assignworker from './Pages/Admin/Assignworker.tsx'
-import Worker from './Pages/Admin/Worker.tsx'
-import Newcurse from './Pages/Admin/Course/Newcurse.tsx'
-import Newmodule from './Pages/Admin/Module/Newmodule.tsx'
-import Newclass from './Pages/Admin/Class/Newclass.tsx'
-import Newworker from './Pages/Admin/Newworker.tsx'
+
 import Course from './Pages/Admin/Course.tsx'
+import Newcurse from './Pages/Admin/Course/Newcurse.tsx'
+
 import Module from './Pages/Admin/Module.tsx'
+import Newmodule from './Pages/Admin/Module/Newmodule.tsx'
+
 import Class from './Pages/Admin/Class.tsx'
+import Newclass from './Pages/Admin/Class/Newclass.tsx'
+
 import Resource from './Pages/Admin/Resource.tsx'
 import Resourcelink from './Pages/Admin/Newresourcelink.tsx'
+
+import Newworker from './Pages/Admin/Newworker.tsx'
+import Worker from './Pages/Admin/Worker.tsx'
+import Assignworker from './Pages/Admin/Assignworker.tsx'
+
 import Newproyect from './Pages/Admin/Newproject.tsx'
+
 import MisCursos from './Pages/User/panelUsuario.tsx'
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-      <Routes>
+  <BrowserRouter>
+    <Routes>
 
       <Route index element={<Login />} />
 
+      <Route path="/admin" element={<AdminLayout />} >
+        <Route path="Register" element={<Newworker />} />
 
+        <Route path="courses">
+          <Route index element={<Course />} />
+          <Route path="add" element={<Newcurse />} />
+          <Route path=":courseId">
+            <Route index element={<Module />} />
+            <Route path="add" element={<Newmodule />} />
+            <Route path=":moduleId">
+              <Route index element={<Class />} />
+              <Route path="add" element={<Newclass />} />
+              <Route path=":classId">
+                <Route index element={<Resource />} />
+                <Route path="add" element={<Resourcelink />} />
+              </Route>
+            </Route>
+          </Route>
+        </Route>
 
-        <Route path="/admin" element={<AdminLayout />} >
-          <Route path="Register" element={<Newworker />} />
-          <Route path="Courses" >
-            <Route path="Add" element={<Newcurse />} />
-          </Route>
-          <Route path="Modules/:courseId">
-            <Route path="Add" element={<Newmodule />} />
-          </Route>
-          <Route path="Classes/:courseId">
-            <Route path=":moduleId/Add" element={<Newclass />} />
-          </Route>
-          <Route path="Worker" element={<Worker />} />
-          <Route path="Newmodule" element={<Newmodule />} />
-          <Route path="Newclass" element={<Newclass />} />
-          <Route path="Resource" element={<Resource />} />
-          <Route path="Resourcelink" element={<Resourcelink />} />
-          <Route path="Newproyect" element={<Newproyect />} />
-          <Route path="Assignworker" element={<Assignworker />} />
-          <Route path="PanelUsuario" element={<MisCursos />} >
-        <Route path="Assignworker" element={<Assignworker />} />
         <Route path="Worker" element={<Worker />} />
-        <Route path="Newworker" element={<Newworker />} />
-        <Route path="Course" element={<Course />} />
-        <Route path="Newcourse" element={<Newcurse />} />
-        <Route path="Module" element={<Module />} />
-        <Route path="Newmodule" element={<Newmodule />} />
-        <Route path="Class" element={<Class />} />
-        <Route path="Newclass" element={<Newclass />} />
-        <Route path="Resource" element={<Resource />} />
-        <Route path="Newresource" element={<Resource />} />
-        <Route path="Newresourcelink" element={<Resourcelink />} />
-        <Route path="Project" element={<Newproyect />} />
-        <Route path="Newproject" element={<Assignworker />} />
-          </Route>
-        </Route>
+        <Route path="Newproyect" element={<Newproyect />} />
+        <Route path="Assignworker" element={<Assignworker />} />
+      </Route>
 
-        <Route path="/employee" element={<EmployeeLayout />} >
+      <Route path="/employee" element={<EmployeeLayout />} >
         <Route path="PanelUsuario" element={<MisCursos />} />
+      </Route>
 
-        </Route>
-
-      </Routes>
-    </BrowserRouter>,
+    </Routes>
+  </BrowserRouter>,
 )
