59e1064973d98b1e70f0e420435510640f3639da
Carlos Galindo <utm22030587@utma.edu.mx>
2025-03-15 12:57:21 -0600

    fix: redirect to correct route

--- Frontend/src/Pages/User/Course/CustomModule.tsx
+++ Frontend/src/Pages/User/Course/CustomModule.tsx
@@ -8,7 +8,7 @@ export default function CustomModule({ module }: { module: ModuleType }) {
 
     return (
         <div key={module.id} className="bg-white p-4 rounded-lg shadow-md hover:shadow-lg transition duration-300 hover:cursor-pointer"
-            onClick={() => navigate(`/user/course/${courseId}/${module.id}`)}>
+            onClick={() => navigate(`/employee/course/${courseId}/${module.id}`)}>
             <h3 className="text-lg font-semibold text-gray-800">{module.name}</h3>
             <p className="text-gray-600 text-sm mt-2">{module.description}</p>
         </div>
